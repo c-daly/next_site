c079d9b8d84d7237ef5179a6385a87fae4cbc10f
c-daly <chris@cdaly.me>
2025-11-21 22:58:43 -0500

    google tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navigation from "@/components/Navigation";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -20,6 +21,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MMDB3YGZE6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MMDB3YGZE6');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} antialiased`}>
         <Navigation />
         <main>{children}</main>
